8c112b985fe140ac17a857169010ca375f89aa5d
ArturGuilherme <silvasaick@gmail.com>
2024-03-07 12:51:28 -0300

    update-code

--- app.py
+++ app.py
@@ -1,36 +1,87 @@
-from integration import integration
-from extraction import extraction
-from time import sleep
-import yaml
-import os
-import cronitor
+import PIconnect as PI
+import pandas as pd
+
+from libs import (
+    get_list_process,
+    get_periods,
+    get_values_by_tag,
+    get_list_tags,
+    prepare_data,
+    send_data,
+    init_variables,
+)
+
+
+# set timezone for PIMS System
+PI.PIConfig.DEFAULT_TIMEZONE = "America/Sao_Paulo"
+
 
 def main():
-    
-    # current directory
-    cwd = os.getcwd()
 
-    # get tags configuration for conitor.io and PIMS systems
-    with open(cwd + "/config/config.yaml") as file:
-        documents = yaml.full_load(file)
-        api_key_monitor = documents["monitor-api-key"]
-        server_name = documents["server-historiador"]
-        monitor_client = documents["monitor-client"]
+    futurai_api_key, futurai_url_api, futurai_company_id, server_name = init_variables()
 
-    # set cronitor.io
-    cronitor.api_key = api_key_monitor
-    monitor = cronitor.Monitor(monitor_client)
+    all_process = get_list_process()
 
-    # start extraction
-    monitor.ping(state="run",message="Iniciando extração...")
-    extraction(monitor,server_name)
+    # connect with PIMS System
+    with PI.PIServer(server=server_name) as server:
 
-    sleep(1)
+        # loop by process
+        for process in all_process:
 
-    # start Integration
-    integration(monitor)
-    monitor.ping(state="complete",message="Finalizando integração...")
+            process_name = process
+            process_id = all_process[process]
+
+            all_periods = get_periods(
+                company_id=futurai_company_id,
+                process_id=process,
+                url=futurai_url_api,
+                futurai_api_key=futurai_api_key,
+            )
+
+            for period in all_periods:
+
+                # init dataframe master of process
+                df_master = pd.DataFrame([])
+                df_master = get_values_by_tag(
+                    server, tags[0], period["start_time"], period["end_time"]
+                )
+                df_master.reset_index(inplace=True)
+                df_master.columns = ["TIMESTAMP", tags[0]]
+                tags.pop(0)
+
+                tags = get_list_tags(process_name)
+
+                # loop by tag
+                for tag in tags:
+
+                    # get values by tag
+                    df_aux = get_values_by_tag(
+                        server, tag, period["start_time"], period["end_time"]
+                    )
+                    df_aux.reset_index(inplace=True)
+                    df_aux.columns = ["TIMESTAMP", tag]
+                    df_aux.drop("TIMESTAMP", axis=1, inplace=True)
+
+                    # merge dataframe master with dataframe aux
+                    df_master = df_master.merge(
+                        df_aux, left_index=True, right_index=True
+                    )
+
+                df_master.set_index("TIMESTAMP", inplace=True)
+
+                file_name, data_json = prepare_data(df_master, process_id=process_id)
+
+                response = send_data(
+                    company_id=futurai_company_id,
+                    process_id=process_id,
+                    url=futurai_url_api,
+                    futurai_api_key=futurai_api_key,
+                    file_name=file_name,
+                    data_json=data_json,
+                )
+
+                print(response)
 
 
-if __name__ == '__main__':
-   main()
+if __name__ == "__main__":
+    main()
---
+++ libs.py
@@ -0,0 +1,121 @@
+import requests
+import json
+from datetime import datetime
+from collections import ChainMap
+import os
+import yaml
+
+cwd = os.getcwd()
+
+
+def init_variables():
+    # get tags configuration for cronitor.io and PIMS systems
+    with open(cwd + "/config/config.yaml") as file:
+        documents = yaml.full_load(file)
+
+        server_name = documents["SERVER-HISTORIADOR"]
+        futurai_api_key = documents["FUTURAI-API-KEY"]
+        futurai_url_api = documents["FUTURAI-URL-API"]
+        futurai_company_id = documents["FUTURAI-COMPANY-ID"]
+
+    return futurai_api_key, futurai_url_api, futurai_company_id, server_name
+
+
+def get_periods(url, company_id, process_id, futurai_api_key):
+    url_path = url + "periods"
+    params = {"company_id": company_id, "process_id": process_id}
+
+    headers = {"x-api-key": futurai_api_key}
+
+    response = requests.get(url_path, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("Falha na chamada à API:", response.status_code)
+        return None
+
+
+def get_list_process():
+    """Get the list of process in yaml configuration file
+    Params: none
+    Returns:
+        - process: list of strings"""
+
+    with open(cwd + "/config/config.yaml") as file:
+        documents = yaml.full_load(file)
+        documents = documents["PROCESS"]
+        process = dict(ChainMap(*documents))
+
+    return process
+
+
+def get_list_tags(process):
+    """Get the list of tag by process
+    Params:
+        - process: string
+    Retunrs:
+        - tags: list of strings"""
+
+    with open(cwd + "/config/tags.yaml") as file:
+        documents = yaml.full_load(file)
+        tags = list(documents[process])
+
+    return tags
+
+
+def get_values_by_tag(server, tag, data_ini, data_fin):
+    """Get values by tag
+    Params:
+        - server: string
+        - tag: string
+        - data_ini: string
+        - data_fin: string
+    Retunrs:
+        - DataFrame"""
+
+    # setup tag
+    point = server.search(tag)[0]
+
+    # get de values with interpolated of 1 minute of interval
+    data = point.interpolated_values(data_ini, data_fin, "1m")
+
+    return pd.DataFrame(data)
+
+
+def prepare_data(df_master, process_id):
+    df_master.rename_axis("TIMESTAMP", inplace=True)
+    df_master.reset_index(inplace=True)
+    df_master["TIMESTAMP"] = df_master["TIMESTAMP"].dt.strftime("%Y-%m-%d %H:%M")
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+    file_name = process_id + "_" + dt_string + ".csv"
+    data_json = df_master.to_dict(orient="records")
+
+    return file_name, data_json
+
+
+def send_data(url, company_id, process_id, file_name, futurai_api_key, data_json):
+    url_path = url + "send-data"
+    payload = {
+        "company_id": company_id,
+        "process_id": process_id,
+        "file_name": file_name,
+        "file": data_json,
+    }
+
+    payload = json.dumps(payload)
+
+    headers = {"x-api-key": futurai_api_key, "Content-Type": "application/json"}
+
+    # Enviando a solicitação HTTP com o corpo contendo os parâmetros
+    response = requests.post(url_path, data=payload, headers=headers)
+
+    # Verificando a resposta da solicitação
+    if response.status_code == 200:
+        print("Solicitação enviada com sucesso!")
+        return response.status_code
+    else:
+        print("Ocorreu um erro ao enviar a solicitação:", response.text)
+        return None
